5ae9dc4accc711c6e2f29d91e389626b5912fb46
alvinwastaken404 <eliana122189@gmail.com>
2026-06-23 13:05:42 +0700

    final fix

--- app.js
+++ app.js
@@ -39,6 +39,4 @@ app.use("/", authRoutes);
 app.use("/", pageRoutes);
 app.use("/api", apiRoutes);
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-});
+module.exports = app;
